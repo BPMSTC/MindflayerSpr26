e54a79010006cc5350a43523416bf80bee06a637
Zachary Driscoll <17999474@mstc.edu>
2026-04-04 18:44:40 -0500

    Refactor styles and HTML structure for catmon selection and adventure components. Unifying styles across all pages and following WCAG guidelines for visual accessibility

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,24 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< Updated upstream
+// <<<<<<< Updated upstream
 
 import { routes } from './app.routes';
-=======
+//=======
 //import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
-import { routes } from './app.routes';
+//import { routes } from './app.routes';
 //import { jwtInterceptor } from './@core/interceptors/jwt.interceptor';
 //import { errorInterceptor } from './@core/interceptors/error.interceptor';
->>>>>>> Stashed changes
+// >>>>>>> Stashed changes
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-<<<<<<< Updated upstream
+//<<<<<<< Updated upstream
+//    provideRouter(routes)
+//=======
     provideRouter(routes)
-=======
-    provideRouter(routes),
 //    provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor]))
->>>>>>> Stashed changes
+// >>>>>>> Stashed changes
   ]
 };
